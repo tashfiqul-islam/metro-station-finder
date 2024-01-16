7cbfc3a1ce72cde6164a4261320b4a6f88fdf348
tashfiqul-islam <tashfiq61@gmail.com>
2024-01-16 20:59:01 +0600

    fix(updated next-config)

--- next.config.cjs
+++ next.config.cjs
@@ -1,6 +1,8 @@
 import withImages from 'next-images';
 
 export default withImages({
+  // Enable static HTML export
+  output: 'out',
   env: {
     NEXT_PUBLIC_GOOGLE_MAPS_API_KEY:
       process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY,
