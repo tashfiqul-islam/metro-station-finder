b9babe80be0cfcf035688b2ee3653a954211165c
tashfiqul-islam <tashfiq61@gmail.com>
2024-01-16 21:44:36 +0600

    update(removing image config)

--- next.config.mjs
+++ next.config.mjs
@@ -8,9 +8,6 @@ const nextConfig = {
     NEXT_PUBLIC_GOOGLE_MAPS_API_KEY:
       process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY,
   },
-  images: {
-    disableStaticImages: true, // Disable Image Optimization
-  },
 };
 
 export default nextConfig;
